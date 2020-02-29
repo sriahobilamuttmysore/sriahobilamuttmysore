b7482f29bfb4c28dac119bc2373edb6e67ec87ad
gautam1858 <gautamrbharadwaj@gmail.com>
2020-02-29 08:13:57 +0530

    design of the website

---
+++ wpscripts/global_navtree.js
@@ -0,0 +1,62 @@
+var global_navtree = WpNavBar.readTree({
+"childArray" : [
+{   sDescription:'Welcome to Mataji\'s Temple.',
+    sTitle:'Home',
+    bIsWebPath:true,
+    sUrl:'index.html'
+},
+{   sTitle:'News & Events',
+    bIsWebPath:true,
+    sUrl:'events.html'
+},
+{   sDescription:'Photo Gallery of Mataji\'s Temple.',
+    sTitle:'Gallery',
+    bIsWebPath:true,
+    sUrl:'gallery.html'
+},
+{   sTitle:'Mataji\'s Aarti',
+    bIsWebPath:true,
+    sUrl:'aarti.html'
+},
+{   sDescription:'Hanuman Chalisa',
+    sTitle:'Hanuman Chalisa',
+    bIsWebPath:true,
+    sUrl:'hanuman_chalisa.html'
+},
+{   sDescription:'General Information about Mataji Temple.',
+    sTitle:'Information',
+    bIsWebPath:true,
+    sUrl:'information.html'
+},
+{   sDescription:'Priest Services',
+    sTitle:'Priest Services',
+    bIsWebPath:true,
+    sUrl:'service.html'
+},
+{   sDescription:'About Mataji',
+    sTitle:'About Mataji',
+    bIsWebPath:true,
+    sUrl:'aboutmataji.html'
+},
+{   sTitle:'About Us',
+    bIsWebPath:true,
+    sUrl:'aboutus.html'
+},
+{   sTitle:'Donation',
+    bIsWebPath:true,
+    sUrl:'donation.html'
+},
+{   sTitle:'Horoscope',
+    bIsWebPath:true,
+    sUrl:'horoscope.html'
+},
+{   sDescription:'Contact Us at Mataji\'s Temple',
+    sTitle:'Contact Us',
+    bIsWebPath:true,
+    sUrl:'contact.html'
+},
+{   sTitle:'easyfundraising',
+    bIsWebPath:true,
+    sUrl:'easyfund.html'
+}]
+});
